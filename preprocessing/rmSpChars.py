69ca76c9e2f7f2165a836e5beb297c196d73445f
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-05-01 21:40:49 -0400

    Add files via upload

--- preprocessing/rmSpChars.py
+++ preprocessing/rmSpChars.py
@@ -1,17 +1,22 @@
+import re
 import string
 from cleanflow.assertions import *
 from pyspark.sql.functions import col, udf
 from pyspark.sql.types import StringType
 
-def rmSpChars(df, columns="*"):
+def rmSpChars(df, columns="*", regex=None):
     """
-    This function remove special chars in string columns, such as: .$%()!&"#/ etc
+    This function remove special characters in string columns, such as: .$%()!&"#/
+    You can also remove unwanted sub-string by specifying the regex in "regex" parameter!
     
     Parameters
     ----------
-    columns : list of columns names to be processed.
-                columns argument can be a string or a list of strings."""
-
+    columns : list of names columns to be processed.
+    columns : argument can be a string or a list of strings.
+    regex   : string that contains the regular expression
+    
+    return df
+    """
     assert_type_str_or_list(df, columns, "columns")
     valid_cols = [c for (c, t) in filter(lambda t: t[1] == 'string', df.dtypes)]
 
@@ -27,11 +32,15 @@ def rmSpChars(df, columns="*"):
 
     assert (col_not_valids == set()), 'Error: The following columns do not have same datatype argument provided: %s' % col_not_valids
 
-    def rm_Sp_Chars(inputStr):
-        for punct in (set(inputStr) & set(string.punctuation)):
-            inputStr = inputStr.replace(punct, "")
+    def rm_Sp_Chars(inputStr, regex):
+        if regex is None:
+            for punct in (set(inputStr) & set(string.punctuation)):
+                inputStr = inputStr.replace(punct, "") 
+        else:
+            for _ in set(inputStr):
+                inputStr = re.sub(regex, '', inputStr)
         return inputStr
 
-    function = udf(lambda cell: rm_Sp_Chars(cell) if cell is not None else cell, StringType())
+    function = udf(lambda cell: rm_Sp_Chars(cell, regex) if cell is not None else cell, StringType())
     exprs = [function(c).alias(c) if (c in columns) and (c in valid_cols)  else c for c in df.columns]
     return df.select(*exprs)
