1ee1546224a9a327bcec96ccf0287976a4fae656
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-05-05 19:44:37 -0400

    Update format_date.py

--- cleanflow/preprocessing/format_date.py
+++ cleanflow/preprocessing/format_date.py
@@ -4,10 +4,13 @@ from pyspark.sql.functions import date_format, unix_timestamp
 
 def format_date(df, columns, current_format, output_format):
     """
-    :param df:      dataframe whose date column has to be modified
-    :param  columns:     Name date columns to be transformed.
-    :param  current_format:   current date string format eg: dd-MM-yyy
-    :param  output_format:    output date string format to be expected.
+    
+    Parameters
+    ---------
+    df             :      dataframe whose date column has to be modified
+    columns        :     Name date columns to be transformed.
+    current_format :   current date string format eg: dd-MM-yyy
+    output_format  :    output date string format to be expected.
     """
     assert isinstance(current_format, str), "Error, current_format argument provided must be a string."
     assert isinstance(output_format, str), "Error, output_format argument provided must be a string."
@@ -20,7 +23,4 @@ def format_date(df, columns, current_format, output_format):
 
     expressions = [date_format(unix_timestamp(c, current_format).cast("timestamp"), output_format).alias(
         c) if c in columns else c for c in df.columns]
-
-    df = df.select(*expressions)
-
-    return df
+    return df.select(*expressions)
