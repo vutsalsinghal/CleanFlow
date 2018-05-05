6abec56f37d4e8859b8855c0968c40dc19055805
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-05-05 19:42:22 -0400

    Update __init__.py

--- cleanflow/preprocessing/__init__.py
+++ cleanflow/preprocessing/__init__.py
@@ -3,6 +3,7 @@ from .trim_col import trim_col
 from .lower_case import lower_case
 from .upper_case import upper_case
 from .impute_missing import impute_missing
+from .replace_null import replace_null
 from .remove_duplicates import remove_duplicates
 from .rmSpChars import rmSpChars
 from .cleanColumnNames import cleanColumnNames
@@ -10,6 +11,4 @@ from .cast_to import cast_to_int, cast_to_double, cast_to_string
 from .drop_null import drop_null
 from .format_date import format_date
 
-__all__ = ['drop_col', 'trim_col', 'lower_case', 'upper_case', 'impute_missing', 'remove_duplicates',
-           'rmSpChars', 'cleanColumnNames', 'drop_null', 'cast_to_int', 'cast_to_double', 'cast_to_string',
-           'format_date']
+__all__ = ['drop_col', 'trim_col','lower_case' ,'upper_case', 'impute_missing', 'replace_null', 'remove_duplicates', 'rmSpChars', 'cleanColumnNames', 'drop_null', 'cast_to_int', 'cast_to_double', 'cast_to_string', 'format_date']
