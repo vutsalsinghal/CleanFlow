e9e7372a14db9393ad4a349489364e655c50a654
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-05-10 21:57:17 -0400

    minor changes

--- cleanflow/preprocessing/drop_col.py
+++ cleanflow/preprocessing/drop_col.py
@@ -1,3 +1,19 @@
+from cleanflow.assertions import *
+
 def drop_col(df, columns):
+    """
+    Function to remove specific column(s)
+
+    Parameters
+    ----------
+    df
+    column
+    """
+    assert_type_str_or_list(df, columns, "columns")
+
+    if isinstance(columns, str):
+        columns = [columns]
+
+    assert_cols_in_df(df, columns_provided=columns, columns_df=df.columns)
     exprs = filter(lambda c: c not in columns, df.columns)
     return df.select(*exprs)
