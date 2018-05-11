42a8fffc8fdcb340f7697cbb4d34f83639a47e45
Vutsal Singhal <vutsalsinghal@gmail.com>
2018-05-10 21:49:25 -0400

    added feature to return summary too

--- cleanflow/preprocessing/drop_null.py
+++ cleanflow/preprocessing/drop_null.py
@@ -1,4 +1,12 @@
+from cleanflow.assertions import *
 from pyspark.sql.functions import col
+
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+
+sc = SparkContext.getOrCreate()
+sqlContext = SQLContext(sc)
 
 def drop_null(df, column):
     '''
@@ -9,6 +17,13 @@ def drop_null(df, column):
     df     : dataframe
     column : column to be processed
 
-    return df
+    Return
+    ------
+    df     : dataframe
+    summary: summary of action performed (type='pyspark.sql.dataframe.DataFrame')
     '''
-    return df.where(col(column).isNotNull())
+    newDF = df.where(col(column).isNotNull())
+    previousTotRows = df.count()
+    newTotRows = newDF.count()
+    summary = sqlContext.createDataFrame([(previousTotRows, newTotRows, previousTotRows-newTotRows)],['Previous Row Count', 'New Row Count','Rows affected'])
+    return (newDF, summary)
